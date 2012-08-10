b30efee83338593d640d922080370293dcfb4f9c
JensDiemer <git@jensdiemer.de>
2012-08-10 14:09:59 +0200

    catch if user click on a code line

--- djangobb_code_comments/static/code_comments.js
+++ djangobb_code_comments/static/code_comments.js
@@ -1,3 +1,26 @@
+// helper function for console logging
+function log() {
+    if (window.console && window.console.log) {
+        try {
+            window.console.log(Array.prototype.join.call(arguments,''));
+        } catch (e) {
+            log("Error:" + e);
+        }
+    }
+}
+log("code_comments.js loaded");
+
 $(document).ready(function() {
-    // TODO            
+    $("table.codetable .line").click(function() {
+        var obj=$(this);
+        if (obj.length!=1) {
+            log("selecte didn't match!");
+            return
+        }
+        log("click on text:" + obj.text());
+        var raw_class=obj.attr("class");
+        //log("raw_class:" + raw_class);
+        var line_no = raw_class.split(" ")[1];
+        log("line no:" + line_no);
+    });
 });
